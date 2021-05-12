8c0de161b6d696abc0eca87b0c03f57980f2dac7
Litamii <litamidesigns@gmail.com>
2021-05-13 01:18:40 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,287 @@
+(function($) { "use strict";
+
+
+	//Preloader
+
+	Royal_Preloader.config({
+		mode           : 'logo',
+		logo           : 'images/logo.png',
+		logo_size      : [170, 32],
+		showProgress   : true,
+		showPercentage : true,
+        text_colour: '#f8f8f8',
+		background:  '#212121'
+	});
+	
+	
+	//Navigation	
+
+	$('ul.slimmenu').on('click',function(){
+			var width = $(window).width(); 
+			if ((width <= 1200)){ 
+			$(this).slideToggle(); 
+		}	
+	});				
+	$('ul.slimmenu').slimmenu(
+			{
+			resizeWidth: '1200',
+			collapserTitle: '',
+			easingEffect:'easeInOutQuint',
+			animSpeed:'medium',
+			indentChildren: true,
+			childrenIndenter: '&raquo;'
+	});
+	
+	/* Scroll Animation */
+
+      window.scrollReveal = new scrollReveal();
+	
+	//Home text fade on scroll	
+	
+	$(window).scroll(function () { 
+        var $Fade = $('.fade-elements');
+        //Get scroll position of window 
+        var windowScroll = $(this).scrollTop();
+        //Slow scroll and fade it out 
+        $Fade.css({
+            'margin-top': -(windowScroll / 0) + "px",
+            'opacity': 1 - (windowScroll / 300)
+        });
+    });	
+	
+	/* Scroll Too */
+	
+			$(window).load(function(){"use strict";
+				
+				/* Page Scroll to id fn call */
+				$("ul.slimmenu li a,a[href='#top'],a[data-gal='m_PageScroll2id']").mPageScroll2id({
+					highlightSelector:"ul.slimmenu li a",
+					offset: 78,
+					scrollSpeed:800,
+					scrollEasing: "easeInOutCubic"
+				});
+				
+				/* demo functions */
+				$("a[rel='next']").click(function(e){
+					e.preventDefault();
+					var to=$(this).parent().parent("section").next().attr("id");
+					$.mPageScroll2id("scrollTo",to);
+				});
+				
+			});	
+
+
+
+	
+	$(document).ready(function() {
+
+		//Tooltip
+
+		$(".tipped").tipper();
+	
+		/* Separate Carousels */
+	 
+		$("#owl-sep-1").owlCarousel({
+			navigation: false,
+			pagination : true,
+			transitionStyle : "fade",
+			slideSpeed : 500,
+			paginationSpeed : 500,
+			singleItem:true,
+			autoPlay: 5000
+		});
+
+		/* Logos Carousel */		
+		
+		$("#owl-logos").owlCarousel({
+			items : 5,
+			itemsDesktop : [1000,4], 
+			itemsDesktopSmall : [900,3],
+			itemsTablet: [600,2], 
+			itemsMobile : false, 
+			navigation: false,
+			pagination : false,
+			autoPlay : 3000,
+			slideSpeed : 300
+		});
+
+		
+		//Facts Counter 
+	
+        $('.counter-numb').counterUp({
+            delay: 100,
+            time: 3000
+        });
+
+
+		//Parallax
+		
+		$('.parallax-freelance').parallax("50%", 0.3);
+
+
+
+		
+		
+	/* Portfolio Sorting */
+
+		(function ($) { 
+		
+		
+			var container = $('#projects-grid');
+			
+			
+			function getNumbColumns() { 
+				var winWidth = $(window).width(), 
+					columnNumb = 1;
+				
+				
+				if (winWidth > 1500) {
+					columnNumb = 4;
+				} else if (winWidth > 1200) {
+					columnNumb = 3;
+				} else if (winWidth > 900) {
+					columnNumb = 2;
+				} else if (winWidth > 600) {
+					columnNumb = 2;
+				} else if (winWidth > 300) {
+					columnNumb = 1;
+				}
+				
+				return columnNumb;
+			}
+			
+			
+			function setColumnWidth() { 
+				var winWidth = $(window).width(), 
+					columnNumb = getNumbColumns(), 
+					postWidth = Math.floor(winWidth / columnNumb);
+
+			}
+			
+			$('#portfolio-filter #filter a').click(function () { 
+				var selector = $(this).attr('data-filter');
+				
+				$(this).parent().parent().find('a').removeClass('current');
+				$(this).addClass('current');
+				
+				container.isotope( { 
+					filter : selector 
+				});
+				
+				setTimeout(function () { 
+					reArrangeProjects();
+				}, 300);
+				
+				
+				return false;
+			});
+			
+			function reArrangeProjects() { 
+				setColumnWidth();
+				container.isotope('reLayout');
+			}
+			
+			
+			container.imagesLoaded(function () { 
+				setColumnWidth();
+				
+				
+				container.isotope( { 
+					itemSelector : '.portfolio-box-1', 
+					layoutMode : 'masonry', 
+					resizable : false 
+				} );
+			} );
+			
+			
+		
+			
+		
+			$(window).on('debouncedresize', function () { 
+				reArrangeProjects();
+				
+			} );
+			
+		
+		} )(jQuery);
+
+
+		
+	});	
+
+ 
+ 
+	/* DebouncedResize Function */
+		(function ($) { 
+			var $event = $.event, 
+				$special, 
+				resizeTimeout;
+			
+			
+			$special = $event.special.debouncedresize = { 
+				setup : function () { 
+					$(this).on('resize', $special.handler);
+				}, 
+				teardown : function () { 
+					$(this).off('resize', $special.handler);
+				}, 
+				handler : function (event, execAsap) { 
+					var context = this, 
+						args = arguments, 
+						dispatch = function () { 
+							event.type = 'debouncedresize';
+							
+							$event.dispatch.apply(context, args);
+						};
+					
+					
+					if (resizeTimeout) {
+						clearTimeout(resizeTimeout);
+					}
+					
+					
+					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
+				}, 
+				threshold : 150 
+			};
+		} )(jQuery);
+
+
+
+	
+	
+  })(jQuery); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+	
